b48ded4e5e1a7e15be5b6a8b006db8b30d8a1903
ADITYA VAISH <f20170379@pilani.bits-pilani.ac.in>
2020-05-30 09:56:34 +0530

    NSG all changes

--- azure files and all.ps1
+++ azure files and all.ps1
@@ -1,7 +1,7 @@
 #Variables
 
 $ResourceGroup = 'DemoRG'
-$VirtualNetwork = 'DemoVnet'
+$VNet = 'DemoVnet'
 $Location = 'canadacentral'
 $VM = 'DemoVM'
 $Image = 'UbuntuLTS'
@@ -14,6 +14,8 @@ $AvailabilitySet = 'DemoAS'
 $VnetAddress = '10.0.0.0/16'
 $SubNet = 'DemoSubnet'
 $SubNetAddress = '10.0.1.0/24'
+$PublicIPName = 'DemoIP'
+$NSG = 'MyNSG'
 # Create a resouce group
 az group create --name $ResourceGroup --location $Location
 
@@ -32,3 +34,40 @@ az vm get-instance-view --name $VM --resource-group $ResourceGroup --query insta
 # create a vnet
 az network vnet create --resource-group $ResourceGroup --name $VNet --address-prefix $VnetAddress --subnet-name $SubNet --subnet-prefix $SubNetAddress
 
+# create a subnet
+az network vnet subnet create --resource-group $ResourceGroup --vnet-name $VNet --name $SubNet --address-prefix $SubNetAddress
+
+# deallocating a VM
+az vm deallocate --resource-group $ResourceGroup --name $VM
+
+# create an IP
+az network public-ip create --resource-group $ResourceGroup --name $PublicIPName
+
+# Alocate a VM to an IP (Using static allocation rather than default dynamic allocation)
+az network public-ip update --resource-group $ResourceGroup --name $PublicIPName --allocation-method static
+
+# Start a VM
+az vm start --resource-group $ResourceGroup --name $VM --no-wait
+
+# Create an NSG
+az network nsg create --resource-group $ResourceGroup --name $NSG
+
+# Update a subnet with an NSG
+az network vnet subnet update --resource-group $ResourceGroup --vnet-name $VNet --name $SubNet --network-security-group $NSG
+
+# New NSG Rule
+az network nsg rule create \
+  --resource-group $ResourceGroup \
+  --nsg-name $NSG \
+  --name http \
+  --access allow \
+  --protocol Tcp \
+  --direction Inbound \
+  --priority 200 \
+  --source-address-prefix "*" \
+  --source-port-range "*" \
+  --destination-address-prefix "*" \
+  --destination-port-range 80
+
+# Output current NSG Rule list
+az network nsg rule list --resource-group $ResourceGroup --nsg-name $NSG --output table
