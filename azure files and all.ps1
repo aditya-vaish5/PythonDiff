895a3cd76e8249d0c1cb36592c33a33606dabd99
ADITYA VAISH <f20170379@pilani.bits-pilani.ac.in>
2020-05-30 10:34:14 +0530

    Peering update

--- azure files and all.ps1
+++ azure files and all.ps1
@@ -8,12 +8,17 @@ $Image = 'UbuntuLTS'
 $User = 'adityav'
 $Password = 'BDt2$Am7&yadd'
 $VNet = 'DemoVNet'
+$VNet2 = 'DemoVNet2'
+$PeeringName = 'Vnet1toVnet2'
 $Address = '10.0.1.0/24'
 $VMSize = 'Standard_B1s'
 $AvailabilitySet = 'DemoAS'
 $VnetAddress = '10.0.0.0/16'
+$Vnet2Address = '10.1.0.0/16'
 $SubNet = 'DemoSubnet'
 $SubNetAddress = '10.0.1.0/24'
+$SubNet2 = 'DemoSubnet2'
+$SubNetAddress2 = '10.1.1.0/24'
 $PublicIPName = 'DemoIP'
 $NSG = 'MyNSG'
 # Create a resouce group
@@ -33,6 +38,12 @@ az vm get-instance-view --name $VM --resource-group $ResourceGroup --query insta
 
 # create a vnet
 az network vnet create --resource-group $ResourceGroup --name $VNet --address-prefix $VnetAddress --subnet-name $SubNet --subnet-prefix $SubNetAddress
+
+# create another Vnet
+az network vnet create --resource-group $ResourceGroup --name $VNet2 --address-prefix $Vnet2Address --subnet-name $SubNet2 --subnet-prefix $SubNetAddress2
+
+# Peer 2 vnet (Vnet1) and (Vnet2)
+az network vnet peering create -g $ResourceGroup -n $PeeringName --vnet-name $VNet --remote-vnet $VNet2 --allow-vnet-access
 
 # create a subnet
 az network vnet subnet create --resource-group $ResourceGroup --vnet-name $VNet --name $SubNet --address-prefix $SubNetAddress
@@ -56,18 +67,8 @@ az network nsg create --resource-group $ResourceGroup --name $NSG
 az network vnet subnet update --resource-group $ResourceGroup --vnet-name $VNet --name $SubNet --network-security-group $NSG
 
 # New NSG Rule
-az network nsg rule create \
-  --resource-group $ResourceGroup \
-  --nsg-name $NSG \
-  --name http \
-  --access allow \
-  --protocol Tcp \
-  --direction Inbound \
-  --priority 200 \
-  --source-address-prefix "*" \
-  --source-port-range "*" \
-  --destination-address-prefix "*" \
-  --destination-port-range 80
+az network nsg rule create --resource-group $ResourceGroup --nsg-name $NSG --name http --access allow --protocol Tcp --direction Inbound --priority 200 --source-address-prefix "*" --source-port-range "*" --destination-address-prefix "*" --destination-port-range 80
 
 # Output current NSG Rule list
 az network nsg rule list --resource-group $ResourceGroup --nsg-name $NSG --output table
+
